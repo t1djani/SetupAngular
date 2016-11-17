fc220b601f3d14ce5fcd968eb2a4643feb0b44d7
t1djani <tidjani.issop@elowise.com>
2016-11-17 10:43:42 +0100

    directive  and alias controller

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,33 @@
-(function () {
-
 'use strict';
 
-  var app = angular.module('SampleApp', ['ngRoute', 'ngAnimate']);
+var app = angular.module('SampleApp', ['ngRoute', 'ngAnimate']);
 
-  app.config(['$locationProvider', '$routeProvider',
-    function($locationProvider, $routeProvider) {
-      $routeProvider
-        .when("/", {
-          templateUrl: "./partials/partials1.html",
-          controller: "MainController"
-        })
-        .otherwise({
-           redirectTo: '/'
-        });
+app.config(['$locationProvider', '$routeProvider',
+  function($locationProvider, $routeProvider) {
+    $routeProvider
+      .when("/", {
+        templateUrl: "./partials/partials1.html",
+        controller: "MainController",
+        controllerAs: "mc"
+      })
+      .otherwise({
+         redirectTo: '/'
+      });
+  }
+]);
+
+app.controller('MainController', function() {
+    this.test = "En cours...";
+    this.name = { name: "Tidjani", lastname: "Issop" };
+  }
+);
+
+app.directive('myTitle', function() {
+  return {
+    restrict: 'E',
+    templateUrl: './partials/partials2.html',
+    scope: {
+      complete: "="
     }
-  ]);
-
-  app.controller('MainController',
-    function($scope) {
-      $scope.test = "En cours...";
-    }
-  );
-
-}());
+  };
+});
